d819a090b6d369d823f7909526e3fc6dc39a9eb1
Renan Victor <80129254+Renanvictordiascosta@users.noreply.github.com>
2024-09-03 15:49:16 -0300

    Add files via upload

---
+++ java/Empregado.java
@@ -0,0 +1,32 @@
+// Classe pública
+public class Empregado {
+
+// Declaração de atributos
+  String id;
+
+  String nome;
+
+  int idade;
+
+  String cargo;
+
+  double salario;
+
+// Construtor da classe que é usado para inicializar os atributos
+  Empregado (String id, String nome, int idade, String cargo, double salario) {
+
+// Parâmetros que inicializam os atributos
+    this.id = id;
+    
+    this.nome = nome;
+    
+    this.idade = idade;
+    
+    this.cargo = cargo;
+    
+    this.salario = salario;
+    
+  }
+      
+}
+ 
---
+++ java/Main.java
@@ -0,0 +1,47 @@
+// Importando a biblioteca Scanner
+import java.util.Scanner;
+
+public class Main {
+
+// Criando o 
+  public static void main(String[] args) {
+
+// Declarando as variáveis
+    Scanner scanner1 = new Scanner(System.in);
+    
+    Scanner scanner2 = new Scanner(System.in);
+    
+    Scanner scanner3 = new Scanner(System.in);
+
+// Chamada dos dados do empregado 1
+    Empregado empregado1 = new Empregado(scanner1.nextLine(), scanner1.nextLine(),                        scanner2.nextInt(),                          scanner1.nextLine(),                         scanner3.nextDouble());
+
+// Chamada dos dados do empregado 2
+    Empregado empregado2 = new Empregado(scanner1.nextLine(), scanner1.nextLine(),                        scanner2.nextInt(),                          scanner1.nextLine(),                         scanner3.nextDouble());
+
+// Print dos dados registrados do empregado 1
+    System.out.println(empregado1.id);
+    
+    System.out.println(empregado1.nome);
+
+    System.out.println(empregado1.idade);
+
+    System.out.println(empregado1.cargo);
+    
+    System.out.println(empregado1.salario);
+
+// Print dos dados registrados do empregado 2
+    System.out.println(empregado2.id);
+
+    System.out.println(empregado2.nome);
+
+    System.out.println(empregado2.idade);
+
+    System.out.println(empregado2.cargo);
+
+    System.out.print(empregado2.salario);
+
+  }
+    
+}
+  
